af0b9170fe5a78e82d52c476fe859170f94755bf
GuardKenzie <tristanferrua@gmail.com>
2021-09-02 02:17:47 +0000

    Removed components from the ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,18 +6,9 @@ module.exports = {
         .setDescription('Replies with Pong!'),
 
     async execute(interaction) {
-
-        const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('again')
-                    .setLabel('Again!')
-                    .setStyle('PRIMARY'),
-            );
-
+        // Pong back
         await interaction.reply({
-            content: 'Pong!',
-            components: [row]
+            content: 'Pong!'
         });
         return true
     },
