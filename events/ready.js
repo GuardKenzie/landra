48e5b4b785e7ab3c03aa8894d4459a12d4b8afb1
GuardKenzie <tristanferrua@gmail.com>
2021-09-04 14:08:45 +0000

    added status

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,18 @@
 const { postEventNotifications, postDailyNotifications } = require("../backend/misc");
 const { setIntervalAsync } = require('set-interval-async/fixed');
 
+const status = "with swords"
+
 module.exports = {
 	name: 'ready',
 	once: true,
 
     async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		// Set status
+		console.log(`Status set to ${status}`);
+		client.user.setActivity(status);
 
 		console.log("\nStarting loops")
 		// Start event notification loop
