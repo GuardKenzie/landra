923f2bcdcd50452a12c19641beeedabe220889a3
Erica <tolvukisa@gmail.com>
2022-10-21 15:23:51 +0000

    Added console error

--- backend/misc.js
+++ backend/misc.js
@@ -320,7 +320,7 @@ async function postEventNotifications(client) {
             // Send message
             await channel.send(message_data)
                 .then(msg => {
-                    setTimeout(() => msg.delete().catch(), 5 * 60 * 1000);
+                    setTimeout(() => msg.delete().catch(console.error()), 5 * 60 * 1000);
                 })
                 .catch(async error => {
                     await sendErrorHandler(error, guild, channel);
